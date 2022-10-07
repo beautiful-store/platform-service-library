7c9f3f39204e4ca14e19d8ee594c92928aa03851
chaos <chaos@chaosui-MacBookAir.local>
2022-10-07 13:36:52 +0900

    #221007 linting

--- aws/sns.go
+++ aws/sns.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 
@@ -9,17 +10,18 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 )
 
-type snsPublishAPI interface {
-	Publish(ctx context.Context,
-		params *sns.PublishInput,
-		optFns ...func(*sns.Options)) (*sns.PublishOutput, error)
-}
+// type snsPublishAPI interface {
+// 	Publish(ctx context.Context,
+// 		params *sns.PublishInput,
+// 		optFns ...func(*sns.Options)) (*sns.PublishOutput, error)
+// }
 
 type awssns struct {
 	client *sns.Client
 	topic  string
 }
 
+// revive:disable:unexported-return
 func NewSNS() *awssns {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
@@ -35,23 +37,22 @@ func (s *awssns) WithTopic(topic string) *awssns {
 		fmt.Println("can't find the topic")
 		return nil
 	}
-	(*s).topic = topic
+	s.topic = topic
 
 	return s
 }
 
-func (s *awssns) Send(message string) {
+func (s *awssns) Send(message string) (*string, error) {
+	var messageID *string
+
 	if s.client == nil {
-		fmt.Println("can't find the client")
-		return
+		return messageID, errors.New("can't find the client")
 	}
 	if len(s.topic) == 0 {
-		fmt.Println("can't find the topic")
-		return
+		return messageID, errors.New("can't find the topic")
 	}
 	if len(message) == 0 {
-		fmt.Println("There is no message")
-		return
+		return messageID, errors.New("There is no message")
 	}
 
 	msgPtr := flag.String("message", message, "")
@@ -68,8 +69,8 @@ func (s *awssns) Send(message string) {
 	if err != nil {
 		fmt.Println("Got an error publishing the message:")
 		fmt.Println(err)
-		return
+		return messageID, err
 	}
 
-	fmt.Println("Message ID: " + *result.MessageId)
+	return result.MessageId, nil
 }
