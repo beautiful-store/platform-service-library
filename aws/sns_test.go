7708a3955a1683a16fe7472a0a4a2af6d779ea2a
chaos <chaos@chaosui-MacBookAir.local>
2022-10-17 11:06:20 +0900

    #221014 send sns with config

--- aws/sns_test.go
+++ aws/sns_test.go
@@ -1,16 +1,29 @@
 package aws
 
 import (
+	"context"
 	"os"
 	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 )
 
 func TestSendSNS(t *testing.T) {
 	topic := os.Getenv("AWSSNS_BEHAVIOR_LOG_TOPIC")
-	if topic == "" {
-		topic = "arn:aws:sns:region:123456789:test_topic"
+	region := os.Getenv("AWS_DEFAULT_REGION")
+	accessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretAcessKeyID := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithDefaultRegion(region),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyID, secretAcessKeyID, "")),
+	)
+	if err != nil {
+		t.Fatal(err.Error())
 	}
-	_, err := NewSNS().WithTopic(topic).Send("this is test")
+
+	_, err = NewSNS(cfg).WithTopic(topic).Send("this is test")
 	if err != nil {
 		t.Fatal(err.Error())
 	}
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/beautiful-store/platform-service-library
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.16.16 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.17.8
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.21 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sns v1.18.1
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/go-xorm/xorm v0.7.9
