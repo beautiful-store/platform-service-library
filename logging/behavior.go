4b7d5e2a0fd108b6bc12f411de929fb94fd1b56e
chaos <chaos@chaosui-MacBookAir.local>
2022-10-28 13:27:57 +0900

    #221020 echo context

--- logging/behavior.go
+++ logging/behavior.go
@@ -175,6 +175,84 @@ func (l *Log) Begin(c echo.Context) {
 	l.Context.Header = string(requestDump)
 }
 
+func (l *Log) Start(c *echo.Context) {
+	l.Context.TimeUnixNano = time.Now().UTC().UnixNano()
+	l.Context.Timestamp = time.Now().Format(layout)
+	l.Context.StartTime = time.Now()
+
+	req := (*c).Request()
+
+	serviceID := (*c).Response().Header().Get(echo.HeaderXRequestID)
+	traceID := serviceID
+	serviceName := "UNKNOWN"
+	for _, r := range (*c).Echo().Routes() {
+		if r.Method == req.Method && r.Path == (*c).Path() {
+			serviceName = r.Name
+			break
+		}
+	}
+
+	realIP := req.RemoteAddr
+	if ip := req.Header.Get(HeaderXForwardedFor); ip != "" {
+		realIP = strings.Split(ip, ", ")[0]
+	} else if ip := req.Header.Get(HeaderXRealIP); ip != "" {
+		realIP = ip
+	} else {
+		realIP, _, _ = net.SplitHostPort(realIP)
+	}
+
+	path := req.URL.Path
+	if path == "" {
+		path = "/"
+	}
+
+	bytesIn, _ := strconv.ParseInt(req.Header.Get(HeaderContentLength), 10, 64)
+
+	var queryString string
+	if len(req.URL.Query()) > 0 {
+		params := make(map[string]interface{}, len(req.URL.Query()))
+		for k, v := range req.URL.Query() {
+			params[k] = v[0]
+		}
+		q, _ := lib.Map2Byte(params)
+		queryString = string(q)
+	}
+
+	var body string
+	b, _ := ioutil.ReadAll(req.Body)
+	req.Body = ioutil.NopCloser(bytes.NewReader(b))
+
+	if b != nil {
+		body = string(b)
+		// bReplaced := passwordRegex.ReplaceAll(b, []byte(`"$1": "*"`))
+		// var bodyParam interface{}
+		// d := json.NewDecoder(bytes.NewBuffer(bReplaced))
+		// d.UseNumber()
+		// if err := d.Decode(&bodyParam); err == nil {
+		// 	body = fmt.Sprintf("%v", bodyParam)
+		// } else {
+		// 	body = string(b)
+		// }
+	}
+
+	requestDump, _ := httputil.DumpRequestOut(req, true)
+
+	l.Context.TraceID = traceID
+	l.Context.ServiceID = serviceID
+	l.Context.ServiceName = serviceName
+	l.Context.RemoteIP = realIP
+	l.Context.URI = req.RequestURI
+	l.Context.Host = req.Host
+	l.Context.Method = req.Method
+	l.Context.Path = path
+	l.Context.Referer = req.Referer()
+	l.Context.UserAgent = req.UserAgent()
+	l.Context.BytesIn = bytesIn
+	l.Context.Query = queryString
+	l.Context.Body = body
+	l.Context.Header = string(requestDump)
+}
+
 func (l *Log) WithMemberID(id int64) *Log {
 	l.Context.MemberID = id
 	return l
@@ -220,8 +298,35 @@ func (l *Log) WriteLog(c echo.Context) {
 		l.Context.BytesOut = res.Size
 	}
 
-	// dumpResponse, _ := httputil.DumpResponse(c.Request().Response, true)
+	// dumpResponse, _ := httputil.DumpResponse(req.Response, true)
 	// l.Body = string(dumpResponse)
+
+	if l.Context.Stack != nil {
+		var traces []string
+		for {
+			trace, err := l.Context.Stack.ReadBytes('\n')
+			if err == io.EOF {
+				break
+			}
+
+			// traces = append(traces, strings.Trim(string(trace), "\n\t"))
+			traces = append(traces, string(trace))
+		}
+		l.Context.StackTrace = strings.Join(traces, "\n")
+	}
+
+	l.Context.Latency = time.Since(l.Context.StartTime).Milliseconds()
+}
+
+func (l *Log) Write(c *echo.Context) {
+	res := (*c).Response()
+	if res != nil {
+		l.Context.Status = res.Status
+		l.Context.BytesOut = res.Size
+	}
+
+	dumpResponse, _ := httputil.DumpResponse((*c).Request().Response, true)
+	l.Context.Header = string(dumpResponse)
 
 	if l.Context.Stack != nil {
 		var traces []string
