b8c180c48ce24bad8543584506b98971db6e30dd
chaos <chaos@chaosui-MacBookAir.local>
2022-11-04 15:26:10 +0900

    #221104 apicall

--- logging/apicall/decode.go
+++ logging/apicall/decode.go
@@ -1,7 +1,21 @@
 package apicall
 
+import (
+	"fmt"
+
+	lib "github.com/beautiful-store/platform-service-library"
+)
+
 func DecodeMessage(message interface{}) *APICall {
-	c := message.(APICall)
+	var c APICall
+
+	s, ok := message.(string)
+	if ok {
+		err := lib.String2Struct(s, &c)
+		if err != nil {
+			fmt.Println("APICall DecodeMessage:", err.Error(), "\n", message)
+		}
+	}
 
 	return &c
 }
--- logging/behavior/decode.go
+++ logging/behavior/decode.go
@@ -1,7 +1,21 @@
 package behavior
 
+import (
+	"fmt"
+
+	lib "github.com/beautiful-store/platform-service-library"
+)
+
 func DecodeMessage(message interface{}) *Log {
-	c := message.(logContext)
+	c := logContext{}
+
+	s, ok := message.(string)
+	if ok {
+		err := lib.String2Struct(s, &c)
+		if err != nil {
+			fmt.Println("Behavior DecodeMessage:", err.Error(), "\n", message)
+		}
+	}
 
 	return &Log{Context: &c}
 }
