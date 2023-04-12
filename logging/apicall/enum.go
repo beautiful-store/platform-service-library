da639027140841e73dca5384977abc9bcfe5dde3
gwiyeomgo <77624394+gwiyeomgo@users.noreply.github.com>
2023-04-12 15:09:22 +0900

    #14 apicall enum 에 회원,계정 서비스 API 유형 추가 (#15)
    Co-authored-by: Gwiyeom Go <gwiy2020@bstore.dooray.com>

--- logging/apicall/enum.go
+++ logging/apicall/enum.go
@@ -10,22 +10,25 @@ const (
 	InternalShaingAPI     typeEnum = 0 // 내부 api 콜
 	InternalOrgSharingAPI typeEnum = 1 // 내부 api 콜
 	InternalTaggingAPI    typeEnum = 2 // 내부 api 콜
-	KakaoLogin            typeEnum = 3 // kakao 로그인
-	KakaoBusinessTalk     typeEnum = 4 // kakao 알람톡
-	KakaoFriendTalk       typeEnum = 5 // kakao 친구톡
-	ExternalCourierCU     typeEnum = 6 // 택배 CU API
-	ExternalCourierGS     typeEnum = 7 // 택배 GS API
+	InternalMemberAPI     typeEnum = 3 // 내부 api 콜
+	InternalAccountAPI    typeEnum = 4 // 내부 api 콜
+	KakaoLogin            typeEnum = 5 // kakao 로그인
+	KakaoBusinessTalk     typeEnum = 6 // kakao 알람톡
+	KakaoFriendTalk       typeEnum = 7 // kakao 친구톡
+	ExternalCourierCU     typeEnum = 8 // 택배 CU API
+	ExternalCourierGS     typeEnum = 9 // 택배 GS API
+
 )
 
 var apiCallTypeEnumValues = [...]string{
-	"InternalShaingAPI", "InternalOrgSharingAPI", "InternalTaggingAPI", // 내부프로젝트
+	"InternalShaingAPI", "InternalOrgSharingAPI", "InternalTaggingAPI", "InternalMemberAPI", "InternalAccountAPI", // 내부프로젝트
 	"KakaoLogin", "KakaoBusinessTalk", "KakaoFriendTalk", // 카카오 관련
 	"ExternalCourierCU", "ExternalCourierGS", // 택배 관련
 }
 
 func (s typeEnum) String() string {
 	switch {
-	case int(s) <= 7:
+	case int(s) <= 9:
 		return apiCallTypeEnumValues[s]
 	default:
 		return fmt.Sprintf("%d", s)
