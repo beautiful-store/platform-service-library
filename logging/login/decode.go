c26af94f4a2037bc3cf8b1c3990df02f0d99e428
chaos <chaoskwon@gmail.com>
2023-07-17 16:55:42 +0900

    #230717-5. error message

--- logging/login/decode.go
+++ logging/login/decode.go
@@ -10,11 +10,14 @@ func DecodeMessage(message interface{}) *Login {
 	var c Login
 
 	s, ok := message.(string)
-	if ok {
-		err := lib.String2Struct(s, &c)
-		if err != nil {
-			fmt.Println("Login DecodeMessage:", err.Error(), "\n", message)
-		}
+	if !ok {
+		fmt.Println("Login DecodeMessage:", "not ok", "\n", message)
+		return nil
+	}
+	err := lib.String2Struct(s, &c)
+	if err != nil {
+		fmt.Println("Login DecodeMessage:", err.Error(), "\n", message)
+		return nil
 	}
 
 	return &c
